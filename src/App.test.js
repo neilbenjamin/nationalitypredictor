e900875f169a1ef04f0b5bb48680796782be9dde
neilbenjamin <neilbenjaminmusic@gmail.com>
2023-10-02 13:35:39 +0200

    October_2nd_Commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,8 @@
 import { render } from "@testing-library/react";
 import App from "./App";
 
-// test("renders learn react link", () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
 test("renders App snapshot", () => {
+  //Utiising the asFragmant property in th render Object which takes a snapshot of HTML structure of the component selected. 
   const { asFragment } = render(<App />);
   expect(asFragment()).toMatchSnapshot();
 });
